61cd683d721207b434cfecd363f4f069174db2e7
jensen-kor <lukawa1@naver.com>
2024-12-19 15:09:01 +0900

    2024-12-19

---
+++ src/components/CartFooter.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Himedia Academy</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/CartHeader.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <Header>
+      <h1>React Cart</h1>
+    </Header>
+  );
+}
+export default Header;
